ffa541e6e6d9091394c6cba97fe8f4f90915759d
RyanDrop <ryangithub7509@gmail.com>
2022-01-17 15:22:34 -0300

    ✨ feat: component angular elements pendent

---
+++ src/app/components/angular-elements/angular-elements.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AngularElementsComponent } from './angular-elements.component';
+
+describe('AngularElementsComponent', () => {
+  let component: AngularElementsComponent;
+  let fixture: ComponentFixture<AngularElementsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AngularElementsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AngularElementsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/angular-elements/angular-elements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mad-angular-elements',
+  templateUrl: './angular-elements.component.html',
+  styleUrls: ['./angular-elements.component.scss']
+})
+export class AngularElementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
