2ad43b439b2d73a06dafac08933aad1cadb4c725
RyanDrop <ryangithub7509@gmail.com>
2022-02-07 09:00:06 -0300

    💄 style: twb remove unused imports

--- src/app/templates/two-way-binding/two-way-binding.component.ts
+++ src/app/templates/two-way-binding/two-way-binding.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mad-two-way-binding',
   templateUrl: './two-way-binding.component.html',
-  styleUrls: ['./two-way-binding.component.scss'],
 })
 export class TwoWayBindingComponent {
   inputValue = 'Ryan';
